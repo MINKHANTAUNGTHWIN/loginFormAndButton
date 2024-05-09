18257aa1d5fb35d47e93429c5f2147499e3d808e
MINKHANTAUNGTHWIN <w22003@osfl.ac.jp>
2024-05-09 23:47:20 +0900

    ssss

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className="">
+        Home
+      </div>
+
+    </div>
+  )
+}
+
+export default page
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,7 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+//markdown file twe use chin tae time use ya tl
+//for security (from unknown attack)
