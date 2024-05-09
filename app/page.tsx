6b06cddc3388c0375a32b1afa70913f6bd021234
MINKHANTAUNGTHWIN <w22003@osfl.ac.jp>
2024-05-09 23:54:29 +0900

    add login form

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,37 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <section className="bg-white h-screen flex justify-center items-center">
-      <div className="flex flex-col md:flex-row items-center">
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 md:mb-0 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      <div className="w-full max-w-md p-6 space-y-4 md:space-y-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-xl font-bold leading-tight tracking-tight bg-cyan-500 rounded-tl-lg rounded-tr-lg focus:border-primary-600 w-full p-2.5 text-white md:text-2xl text-center">
+          ログイン
+        </h1>
+        <form
+          className="space-y-4 md:space-y-6"
+          action="#"
         >
-          出勤
-        </button>
+          <div>
+            <label
+              htmlFor="id"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              あなたの ID
+            </label>
+            <input
+              type="id"
+              name="id"
+              id="id"
+              className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="ID を入力してください"
+            />
+          </div>
 
-        <button
-          type="button"
-          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 md:mb-0 me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-        >
-          退勤
-        </button>
+          <button
+            type="submit"
+            className="w-full text-white bg-red-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-extrabold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            確定
+          </button>
+        </form>
       </div>
     </section>
   );
